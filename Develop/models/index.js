22f61cf80ae6eb8e2bd75a58b3a2da4f3cd727b3
Bryan Peens <peensbryan1@gmail.com>
2024-04-19 00:35:23 -0400

    updated models

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,12 +5,24 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category);
 
 // Categories have many Products
+Category.hasMany(Product);
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id', // Use 'product_id' as foreign key
+  otherKey: 'tag_id' // Specify the other key for the relationship
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id', // Use 'tag_id' as foreign key
+  otherKey: 'product_id' // Specify the other key for the relationship
+});
 
 module.exports = {
   Product,
